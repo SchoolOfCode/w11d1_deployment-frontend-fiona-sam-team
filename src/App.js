797354139cef54dedcb98e1ac7a9917e457ff31b
fiona <fiona.kitchen@yahoo.co.uk>
2022-07-04 14:44:43 +0100

    changing text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World! We are Fiona and Sam!
+          Hello World! We are Fiona and Sam! This is our attempt automatically updating our deployed page  
         </a>
       </header>
     </div>
