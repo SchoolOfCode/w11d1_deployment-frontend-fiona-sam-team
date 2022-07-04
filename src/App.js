1dc50d133219f399c768e4cbb51989dd4adc319f
fiona <fiona.kitchen@yahoo.co.uk>
2022-07-04 16:10:10 +0100

    free code camp form input

--- src/App.js
+++ src/App.js
@@ -9,17 +9,25 @@ function App() {
         <p>
     hello it's me your looking for 
         </p>
-        <form name="contact" netlify>
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+        <form name="contact" method="post">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label htmlFor="name">Name</label> <br />
+        <input type="text" id="name" name="name" required />
+      </p>
+      <p>
+        <label htmlFor="email">Email</label> <br />
+        <input type="email" id="email" name="email" required />
+      </p>
+      <p>
+        <label htmlFor="message">Message</label> <br />
+        <textarea id="message" name="message" required></textarea>
+      </p>
+      <p>
+        <input type="submit" value="Submit message" />
+      </p>
+    </form>
+
         <a
           className="App-link"
           href="https://reactjs.org"
